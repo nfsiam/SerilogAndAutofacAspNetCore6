3b7fb4af684cec8b4d250de460d1f7507ec1062e
nfsiam <iamthenfs@gmail.com>
2021-11-12 18:39:34 +0600

    Integrated Serilog

--- SerilogAndAutofacAspNetCore6/Program.cs
+++ SerilogAndAutofacAspNetCore6/Program.cs
@@ -1,27 +1,62 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
+using Serilog.Events;
+
+var webAppBuilder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+webAppBuilder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(webAppBuilder.Environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{webAppBuilder.Environment.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .ReadFrom.Configuration(configBuilder)
+    .CreateLogger();
+
+try
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    Log.Information("Application starting up");
+
+    webAppBuilder.Host.UseSerilog();
+
+    // Add services to the container.
+    webAppBuilder.Services.AddRazorPages();
+
+    var app = webAppBuilder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application startup failed");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
